98366cfd2acf260cdf7905b3486d251fb33be148
Thai Thien <tthien@apcs.vn>
2015-02-02 19:27:03 +0700

    TODO
    E-commerce Awareness as pie chart
    
    Buy freq as histogram

--- draw_histogram.R
+++ draw_histogram.R
@@ -2,4 +2,11 @@ data<-read.csv("data.csv")
 know<-as.vector(data[[2]])
 hist(know,nclass=5)
 know_hist<-hist(know,nclass=5)
-plot(know_hist,border="dark blue",col="red",main = "Mức độ hiểu biết về TMĐT",xlab="Mức độ")
+plot(know_hist,border="dark blue",col="red",main = "E-commerce Awareness",xlab="None   Low   Medium   High   Extreme",ylab="Respondent" )
+
+
+#TODO buy freq
+buy<-as.vector(data[[3]])
+
+buy_hist<-hist(buy,nclass=4)
+plot(know_hist,border="dark blue",col="red",main = "Số giao dịch thực hiện mỗi tuần",xlab="Giao Dich",ylab="số người" )
---
+++ to_factor.R
@@ -0,0 +1,8 @@
+to_factor<-function(input)
+{
+  lv<-c("Chưa bao giờ","it hon 1","tu 1-3","5 - 7", "7 tro len")
+ 
+  output<-factor(input,lv)
+  output
+  
+}
